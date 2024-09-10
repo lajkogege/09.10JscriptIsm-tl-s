54749148f495bc6cac7870c3f7f3bada08b3a6ad
lajkogege <lajkogege@gmail.com>
2024-09-10 11:46:45 +0200

    gomb eesemény

--- view/Kartya.js
+++ view/Kartya.js
@@ -1,27 +1,43 @@
-export  default class Kartya{
+export default class Kartya {
     //adattagok
-    #obj={} //privát adattag
+    #obj = {} //privát adattag
     #szuloELem;
     //constructor
-    constructor(obj, szuloELem){
-        this.#obj=obj
-        this.#szuloELem=szuloELem
+    constructor(obj, szuloELem) {
+        this.#obj = obj
+        this.#szuloELem = szuloELem
         this.KutyaKiir()
         this.KutyaKiir()
+        //gombelem a Kartya saját adattagja lesz
+        this.gombElem = $(".kivalasztgomb:last") //mindig az utolso kiválasztott gomb legyen ezt csinálja a last 
+        console.log(this.gombElem)
+        this.esemenyKezelo()
     }
     //tagfügvény az adattagokon dolgoznak
-    KutyaKiir(){
+    KutyaKiir() {
         //egyetlen egy kutya kiirása
-       //const tartalomElem=$(".tartalom");
+        //const tartalomElem=$(".tartalom");
         this.#szuloELem.append(`
             <div class="card col-lg-4 col-md-6">
                 <div class="card-body >
                     <h3 class="card-title" >${this.#obj.nev}</h3>
                     <p>${this.#obj.kor}</p>
                     <p>${this.#obj.nev}</p>
-                    <button class=" btn btn-succes">Kiválaszt</button>
+                    <button class=" kivalasztgomb btn btn-succes">Kiválaszt</button>
                 </div>
             `)
-   
+
+    }
+
+    //gombeseények kezelése
+    esemenyKezelo() {
+        this.gombElem.on("click", function(){
+            console.log(this )
+        }
+           
+        )
     }
 }
+
+//this jelentése: function névtelen fügvény esetén a this azta a html dom elemet jelenti, ami az eseményt kiváltota, vagy épepen amelyikre hivatkozunk
+//nyil fv. esetén a this arra az osztály példányra muatatt amit az adott nyil fv közbe fogja
